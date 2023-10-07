2340a332e02cfa0dcefc44a047629ec03d02b41d
iro tochukwu samuel <irotochukwu@gmail.com>
2023-10-07 19:19:37 +0100

    chore : update country and state API

--- application/__init__.py
+++ application/__init__.py
@@ -27,7 +27,7 @@ app.config['SECRET_KEY'] = SECRET_KEY
 app.config["ENVIRONMENT"] = os.environ.get("environment", "development")
 environment = str(app.config["ENVIRONMENT"]).lower()
 
-if environment != "development":
+if environment != "local":
     dictConfig({
         'version': 1,
         'disable_existing_loggers': False,
