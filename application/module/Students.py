eef7f7a353f658287edd67936bf2a72fe4f7a7fd
iro tochukwu samuel <irotochukwu@gmail.com>
2023-10-16 18:26:13 +0100

    chore : update model

--- application/module/Students.py
+++ application/module/Students.py
@@ -26,7 +26,8 @@ class StudentModel:
                     **(res.user.as_dict() if res.user else {}),
                     **res.to_dict(),
                     "project": res.projects if res.projects else [],
-                    "parent": {**(res.parents.to_dict() if res.parents else {}) , **(res.parents.user.as_dict() if res.parents else {})},
+                    "parent": {**(res.parents.to_dict() if res.parents else {}),
+                               **(res.parents.user.as_dict() if res.parents else {})},
                     "school": res.schools.name,
                 } for res in results]
             }
