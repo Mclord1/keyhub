cdd4a7f566b624386f0919e97639aa94b910f471
iro tochukwu samuel <irotochukwu@gmail.com>
2023-11-06 04:36:23 +0100

    chore : restructure code

--- application/api/keywordAPI.py
+++ application/api/keywordAPI.py
@@ -1,11 +1,9 @@
-import sqlalchemy.exc
 from flask import Blueprint, request
 
-from application import db, return_json, OutputObj
+from application import return_json, OutputObj
 from application.Enums.Permission import PermissionEnum
-from application.models.smeModel import Keywords
+from application.module.Keywords import KeywordModel
 from application.utils.authenticator import authenticate
-from exceptions.custom_exception import CustomException
 
 keywords_bp = Blueprint("keywords", __name__)
 
@@ -15,27 +13,14 @@ keywords_bp = Blueprint("keywords", __name__)
 @authenticate(PermissionEnum.ADD_KEYWORD)
 def create_keyword():
     data = request.get_json()
-    if "name" not in data:
-        raise CustomException(message="name is required", status_code=400)
-
-    try:
-        keyword = Keywords(**data)
-        keyword.save(refresh=True)
-        return return_json(OutputObj(code=201, message="Keyword created successfully"))
-
-    except sqlalchemy.exc.IntegrityError:
-        db.session.rollback()
-        raise CustomException(message="A keyword with that name already exist", status_code=400)
-    except Exception as e:
-        db.session.rollback()
-        raise e
+    KeywordModel.create_keyword(data)
+    return return_json(OutputObj(code=201, message="Keyword created successfully"))
 
 
 @keywords_bp.route("/all", methods=["GET"])
 @authenticate(PermissionEnum.VIEW_KEYWORD)
 def get_all_keywords():
-    keywords = Keywords.query.all()
-    keyword_list = [keyword.to_dict(add_filter=False) for keyword in keywords]
+    keyword_list = KeywordModel.get_all_keywords()
     return return_json(OutputObj(code=200, message="keywords fetched", data=keyword_list))
 
 
@@ -43,31 +28,13 @@ def get_all_keywords():
 @keywords_bp.route("/<int:keyword_id>", methods=["PUT"])
 @authenticate(PermissionEnum.MODIFY_KEYWORD)
 def update_keyword(keyword_id):
-    keyword = Keywords.query.filter_by(id=keyword_id).first()
-    if not keyword:
-        raise CustomException(message="Keyword not found", status_code=404)
-
     data = request.get_json()
-    if "name" not in data:
-        raise CustomException(message="name is required", status_code=400)
-
-    try:
-        keyword.update_table(data)
-        db.session.commit()
-        return return_json(OutputObj(code=200, message="Keyword updated successfully"))
-
-    except Exception as e:
-        db.session.rollback()
-        raise e
+    KeywordModel.update_keyword(keyword_id, data)
+    return return_json(OutputObj(code=200, message="Keyword updated successfully"))
 
 
 @keywords_bp.route("/<int:keyword_id>", methods=["DELETE"])
 @authenticate(PermissionEnum.DEACTIVATE_KEYWORD)
 def delete_keyword(keyword_id):
-    keyword = Keywords.query.filter_by(id=keyword_id).first()
-    if not keyword:
-        raise CustomException(message="Keyword not found", status_code=404)
-
-    db.session.delete(keyword)
-    db.session.commit()
+    KeywordModel.delete_keyword(keyword_id)
     return return_json(OutputObj(code=200, message="Keyword deleted successfully"))
--- application/api/smeAPI.py
+++ application/api/smeAPI.py
@@ -1,28 +1,11 @@
-import sqlalchemy.exc
 from flask import Blueprint, request
-from pydantic import BaseModel
 
-from application import db, return_json, OutputObj
+from application import return_json, OutputObj
 from application.Enums.Permission import PermissionEnum
-from application.Schema import validator
-from application.models import School
-from application.models.smeModel import SME
+from application.module.Sme import SmeModel
 from application.utils.authenticator import authenticate, has_school_privilege
-from exceptions.custom_exception import CustomException
 
 sme_bp = Blueprint("sme", __name__)
-
-
-class SMESchema(BaseModel):
-    name: str
-    surname: str
-    email: str
-    contact_telephone: str
-    website: str
-    company_name: str
-    registered_address: str
-    area_of_expertise: str
-    nin_certificate: bool
 
 
 @sme_bp.route("/<int:school_id>", methods=["POST"])
@@ -30,43 +13,15 @@ class SMESchema(BaseModel):
 @has_school_privilege
 def create_sme(school_id):
     sme_data = request.get_json()
-
-    sme: SMESchema = validator.validate_data(SMESchema, sme_data)
-
-    _school = School.GetSchool(school_id)
-
-    try:
-        sme_model = SME(
-            name=sme.name,
-            surname=sme.surname,
-            email=sme.email,
-            contact_telephone=sme.contact_telephone,
-            website=sme.website,
-            company_name=sme.company_name,
-            registered_address=sme.registered_address,
-            area_of_expertise=sme.area_of_expertise,
-            nin_certificate=sme.nin_certificate,
-            schools=_school
-        )
-        db.session.add(sme_model)
-        db.session.commit()
-        return return_json(OutputObj(code=201, message="SME created successfully"))
-
-    except sqlalchemy.exc.IntegrityError:
-        db.session.rollback()
-        raise CustomException(message="An SME with that name or company_name already exist", status_code=400)
-    except Exception as e:
-        db.session.rollback()
-        raise e
+    SmeModel.create_sme(school_id, sme_data)
+    return return_json(OutputObj(code=201, message="SME created successfully"))
 
 
 @sme_bp.route("/<int:school_id>", methods=["GET"])
 @authenticate(PermissionEnum.VIEW_SME)
 @has_school_privilege
 def get_sme(school_id):
-    sme = SME.query.filter_by(school_id=school_id).first()
-    if not sme:
-        raise CustomException(message="SME not found", status_code=404)
+    sme = SmeModel.get_sme(school_id)
     return return_json(OutputObj(code=200, message="SME fetched", data=sme.to_dict(add_filter=False)))
 
 
@@ -74,19 +29,9 @@ def get_sme(school_id):
 @authenticate(PermissionEnum.MODIFY_SME)
 @has_school_privilege
 def update_sme(school_id):
-    _sme: SME = SME.query.filter_by(school_id=school_id).first()
-    if not _sme:
-        raise CustomException(message="SME not found", status_code=404)
-
     data = request.get_json()
-    try:
-        _sme.update_table(data)
-        db.session.commit()
-        return return_json(OutputObj(code=200, message="SME updated successfully"))
-
-    except Exception as e:
-        db.session.rollback()
-        raise e
+    SmeModel.update_sme(school_id, data)
+    return return_json(OutputObj(code=200, message="SME updated successfully"))
 
 
 # Delete an SME by ID
@@ -94,10 +39,5 @@ def update_sme(school_id):
 @authenticate(PermissionEnum.DELETE_SME)
 @has_school_privilege
 def delete_sme(school_id):
-    sme = SME.query.filter_by(school_id=school_id).first()
-    if not sme:
-        raise CustomException(message="SME not found", status_code=404)
-
-    db.session.delete(sme)
-    db.session.commit()
+    SmeModel.delete_sme(school_id)
     return return_json(OutputObj(code=200, message="SME deleted successfully"))
