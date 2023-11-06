71950f844743dbc971e2b4ae68d967d349fb8731
iro tochukwu samuel <irotochukwu@gmail.com>
2023-11-06 01:50:40 +0100

    chore : add sme and keyword

--- config/DBConfig.py
+++ config/DBConfig.py
@@ -3,7 +3,7 @@ DB_SETUP = {
         "username": 'keyhub',
         "password": 'keyhub',
         "host": 'localhost',
-        "port": 5432,
+        "port": 5431,
         'database': 'keyhub'
     },
     "development": {
